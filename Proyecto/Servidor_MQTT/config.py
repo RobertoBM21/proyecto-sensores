b05e7c8f848c1e84ca80db7cd07ae19ca0f2d378
RobertoBM21 <145493843+RobertoBM21@users.noreply.github.com>
2024-05-05 20:54:56 +0200

    Creacion Proyecto

---
+++ Proyecto/Servidor_MQTT/config.py
@@ -0,0 +1,5 @@
+# Datos de configuración de la aplicación (aun por planificar)
+
+# Endpoints Servidores
+# URL de la API del servidor para almacenar los datos
+# Etc.
---
+++ Proyecto/Servidor_MQTT/main.py
@@ -0,0 +1,7 @@
+# Flujo de trabajo (conceptual):
+# 1. Configuración de la aplicación
+# 2. Conexión con los servidores/dispositivos MQTT
+# 3. Recepción y procesamiento de los datos
+# 4. Envío de los datos a la API del servidor
+
+# Posiblemente seran necesarios más archivos para la implementación de la aplicación
