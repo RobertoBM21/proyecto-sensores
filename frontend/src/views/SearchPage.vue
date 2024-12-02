6f5258617232d3ccfdb86d9ff6efa693f6db8374
RobertoBM21 <roberto.burruezom@gmail.com>
2024-12-02 19:10:46 +0100

    Refactor de SearchPage

--- frontend/src/views/SearchPage.vue
+++ frontend/src/views/SearchPage.vue
@@ -1,29 +1,21 @@
-<script>
+<script setup>
 import HeaderComponent from "../components/HeaderComponent.vue";
 import FooterComponent from "../components/FooterComponent.vue";
 import SearchBarComponent from "../components/SearchBarComponent.vue";
 import ResultsComponent from "../components/ResultsComponent.vue";
-
-export default {
-  name: "SearchPage",
-  components: {
-    HeaderComponent,
-    FooterComponent,
-    SearchBarComponent,
-    ResultsComponent,
-  },
-};
 </script>
 
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col">
     <HeaderComponent />
-    <div class="container mx-auto px-4 py-4">
-      <SearchBarComponent />
-      <div class="mt-8">
-        <ResultsComponent />
+    <main class="flex-grow">
+      <div class="container mx-auto px-4 py-4">
+        <SearchBarComponent />
+        <div class="mt-8">
+          <ResultsComponent />
+        </div>
       </div>
-    </div>
+    </main>
     <FooterComponent />
   </div>
 </template>
