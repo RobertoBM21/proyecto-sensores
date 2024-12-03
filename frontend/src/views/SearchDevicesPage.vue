9a62ddec6ae7077fd9e4130752c884ab0e4a008c
RobertoBM21 <roberto.burruezom@gmail.com>
2024-12-03 18:04:30 +0100

    Update SearchDevicesPage.vue
    Patata

--- frontend/src/views/SearchDevicesPage.vue
+++ frontend/src/views/SearchDevicesPage.vue
@@ -3,22 +3,19 @@
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 import SearchBar from "../components/SearchBar.vue";
-import ResultsComponent from "../components/ResultsComponent.vue";
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
     <Header />
-    <main class="flex-grow">
-      <div class="container mx-auto px-4 py-4">
-        <!-- Search Controls -->
-        <h1>Patata</h1>
-        <!-- Results Table & List -->
-        <div class="mt-8">
-          <ResultsComponent
-            @page-change="$refs.searchBar.getDeviceActivityReport()"
-          />
-        </div>
+    <main class="flex-grow flex items-center justify-center">
+      <div class="text-center">
+        <h1
+          class="text-6xl font-bold text-amber-700 hover:text-amber-600 transition-colors duration-300 cursor-pointer"
+        >
+          Patata
+        </h1>
+        <div class="text-8xl mt-12 animate-bounce">🥔</div>
       </div>
     </main>
     <Footer />
