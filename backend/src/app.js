0f30f591e7242c60588500f098ae723caacf48d1
RobertoBM21 <roberto.burruezom@gmail.com>
2025-01-03 12:12:53 +0100

    Actualizacion CORS Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,17 @@ const errorHandler = require("./middleware/errorHandler.js");
 // Crear la aplicación de Express
 const app = express();
 
+// TODO: Configurar CORS y otros middlewares
 // Middleware de seguridad
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
+    credentials: true,
+    maxAge: 86400, //? Cachea preflight CORS por 1 día en el navegador
+  })
+);
 app.use(helmet());
 
 // Middleware para parsear el cuerpo de las peticiones
@@ -25,7 +34,7 @@ app.use("/messages", messageRoutes);
 
 // Middleware para la documentación de la API
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.get("/api-docs.json", (req, res) => {
+app.get("/api-docs.json", (_req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(swaggerSpec);
 });
